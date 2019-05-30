2d28a8b2aec24514d77b49bd81bb281ec7d9681c
dinesh kumar <dinesh4u2k@gmail.com>
2019-05-30 20:58:06 +0530

    third push

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,69 @@ class App extends Component{
     constructor(props){
         super(props);
         this.state={
-            ArtistName:''
+            ArtistName:'',
+            artist: null
         }
+        const params = this.getHashParams();
+        console.log('tokennnnnn',params.access_token);
     }
+
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+           e = r.exec(q);
+        }
+        return hashParams;
+      }
 
     Search(){
         console.log('this state', this.state);
         const BaseUrl = 'https://api.spotify.com/v1/search?';
         const FetchUrl = `${BaseUrl}q=${this.state.ArtistName}&type=artist&limit=1`;
-        let accesstoken = 'BQCIGrEerWhUK5XjhWnIys5crQZOvEEM3fggOO9bdoUPmoP4G8TMttBMoiXc-ykCMCEyKf4TsqMwWGrM0F1h2LNv2iM14AY-gxjRVb70QxidT92uD6kq3FXwbtjsnk_SiE2UIFc6StvrcbJ1oUhjP1g1zRk0O3hNKNk15ziTIbYOtjQXEwhlCeeg'
+        let accesstoken = 'BQBMlk1fu3KVOV_gNeMigSoGCIzzIunCRjIxrev1wu5QuP_CxrpceG6bzl1oPevGR1G6QHYsHUqTKwHEw-h9sUKFPT970frykmFTUMLfj86PlruazQ_clZ5ulWukyBeQrX5v34UaL8kfC5c4FO1ZNG0c2uO-Pt2tySfRPH2Wt3NSz4oqHPrKSGu_';
         console.log('fetch url',FetchUrl);
-        fetch(FetchUrl,{
-            method:'GET',
-            headers:{
-                'Authorization' : 'Bearer' + accesstoken
+        // fetch(FetchUrl,{
+        //     method:'GET',
+        //     headers:{
+        //         'Authorization' : 'Bearer' + accesstoken
+        //     },
+        //     mode: 'cors',
+        //     cache:'default'
+        // })
+        // .then(response => console.log('response', response));
+
+        var myOptions = {
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer ' + accesstoken
             },
             mode: 'cors',
-            cache:'default'
-        })
-        .then(response => console.log('response', response));
+            cache: 'default'
+          };
+      
+          fetch(FetchUrl, myOptions)
+            .then(response => response.json())
+            .then(json => {
+              const artist = json.artists.items[0];        
+              this.setState({ artist });
+            })
+            .then(response => console.log('response', response))
     }
 
     render(){
+        let artist = {
+            name: '',
+            followers: {
+              total: ''
+            }
+          };
+          if (this.state.artist !== null) {
+            artist = this.state.artist;
+          }
         return(
             <div className="App">
                 <div className="title">Musify</div>
@@ -60,7 +101,7 @@ class App extends Component{
 
                 <div className="Gallery">Gallery</div>
             
-                
+                <div>{artist.name}</div>
             
             </div>
         )
