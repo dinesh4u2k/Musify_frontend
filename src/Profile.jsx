84ca255a3a393378213a6d95f2752bac4c46e4b3
dinesh kumar <dinesh4u2k@gmail.com>
2019-05-30 23:27:15 +0530

    profile test

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -15,17 +15,17 @@ class Profile extends Component{
           
           artist = this.props.artist !== null ? this.props.artist : artist;
         return(
-            <div>
+            <div className="profile">
                 <img 
                     alt="profile"
                     className="profile-img"
                     src={artist.images[0].url}
                 />
-                <div className="profile-info">
+             <div className="profile-info">
                 <div className="profile-name">{artist.name}</div>
-                <div className="profile-followers">{artist.followers} Followers</div>
+                <div className="profile-followers">{artist.followers.total} Followers</div>
                 <div className="profile-genres">{
- 
+
                     artist.genres.map((genre,k)=>{
                         genre = genre !== artist.genres[artist.genres.length-1] ? `${genre},` : `& ${genre}`;
                         return(
@@ -34,8 +34,9 @@ class Profile extends Component{
                     })
 
                 }</div>
+                
                 </div>
-            </div>
+        </div>
 
         )
     }
