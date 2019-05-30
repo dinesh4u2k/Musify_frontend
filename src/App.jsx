c88197c4b8fcb184297b54784ac7a4323722e4a9
dinesh kumar <dinesh4u2k@gmail.com>
2019-05-30 20:34:18 +0530

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,15 @@ class App extends Component{
         console.log('this state', this.state);
         const BaseUrl = 'https://api.spotify.com/v1/search?';
         const FetchUrl = `${BaseUrl}q=${this.state.ArtistName}&type=artist&limit=1`;
+        let accesstoken = 'BQCIGrEerWhUK5XjhWnIys5crQZOvEEM3fggOO9bdoUPmoP4G8TMttBMoiXc-ykCMCEyKf4TsqMwWGrM0F1h2LNv2iM14AY-gxjRVb70QxidT92uD6kq3FXwbtjsnk_SiE2UIFc6StvrcbJ1oUhjP1g1zRk0O3hNKNk15ziTIbYOtjQXEwhlCeeg'
         console.log('fetch url',FetchUrl);
         fetch(FetchUrl,{
-            method:'GET'
+            method:'GET',
+            headers:{
+                'Authorization' : 'Bearer' + accesstoken
+            },
+            mode: 'cors',
+            cache:'default'
         })
         .then(response => console.log('response', response));
     }
